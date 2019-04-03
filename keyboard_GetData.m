727db41ded62893d88745c15c48ed80b102804d9
SwapanK <swapanmasanta2011@gmail.com>
2019-04-03 09:03:23 +0530

    Update keyboard_GetData.m

--- keyboard_GetData.m
+++ keyboard_GetData.m
@@ -5,7 +5,7 @@ function UserInput=keyboard_GetData()
 % What happens: GUI based Takes Input
 % Who did: Swapan Masanta - 28-Mar-2019 11:27:36
 
-% Inspired By Sean de Wolski
+% Inspired By
 % https://in.mathworks.com/matlabcentral/answers/8195-on-screen-keyboard?s_tid=answers_rc1-2_p2_MLT
 
 try delete Final.mat
